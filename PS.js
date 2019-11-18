53b92486501448277450a8a1a8168fd540db4c31
wSz1s <shiwuze8@gmail.com>
2019-11-18 15:59:43 +0800

    添加分隔符使文段成为js代码

--- PS.js
+++ PS.js
@@ -1,4 +1,6 @@
-Power shell  V3 could run in windows server2008 &&server2012 &&windows8；
-Powershell is  a very strongful  automatical tool;
-It use cmdlet to  run ;
-alias is easy to understand;
+Power; shell; V3; could;run; inwindows;server2008 &&server2012 &&windows8；
+Powershell; is;; a; very; strongful; automatical; tool;
+It;use;cmdlet;to; run;;
+alias;is;easy;to;understand;
+$('div').text('hi').addclass('red').animate({left:100})
+$fn.Powershell=function(){...}
